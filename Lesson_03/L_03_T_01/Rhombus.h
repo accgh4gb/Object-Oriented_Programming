df7ea82230f089f6fcfc9e1c9f641984a231cda7
Aleksandr <accgh4gb@gmail.com>
2022-11-14 22:30:38 +0600

    The file Rhombus.h was modified

--- Lesson_03/L_03_T_01/Rhombus.h
+++ Lesson_03/L_03_T_01/Rhombus.h
@@ -3,15 +3,15 @@
 class Rhombus : public Parallelogram
 {
 private:
-	unsigned int angle;
-	unsigned int a;
+ 	unsigned int angle;
+ 	unsigned int a;
 
 public:
-	Rhombus(const unsigned int& new_angle = 90, const unsigned int& new_a = 1)
+	Rhombus(const unsigned int& new_angle = 45, const unsigned int& new_a = 1)
 	: angle(new_angle), a(new_a) {}
 	void area()
 	{
-		std::cout << "The area of the rhombus is " << a * a * std::sin(angle * pi / 180) << std::endl;
+	 	std::cout << "The area of the rhombus is " << a * a * std::sin(angle * pi / 180) << std::endl;
 	}
 
 	virtual ~Rhombus() {}
