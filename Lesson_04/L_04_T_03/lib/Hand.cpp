1bda1c4e0536293799e4d08d2c28d8f7091b3dea
Aleksandr <accgh4gb@gmail.com>
2022-11-21 22:59:53 +0600

    The third task of the fourth lesson

---
+++ Lesson_04/L_04_T_03/lib/Hand.cpp
@@ -0,0 +1,42 @@
+#include "Hand.h"
+
+void Hand::Add(Card *pCard)
+{
+	vCards.push_back(pCard);
+}
+
+void Hand::Clear()
+{
+	std::vector<Card*>::iterator it = vCards.begin();
+    while (it != vCards.end())
+    {
+		delete* it;
+		*it = nullptr;
+        ++it;
+    }
+    vCards.clear();
+}
+
+int Hand::GetValue()
+{
+	if (vCards.empty())
+	{
+		return 0;
+    }
+
+    int summ = 0;
+    
+	for (auto it = vCards.begin(); it != vCards.end(); ++it)
+    {
+		if (((*it)->GetValue() == static_cast<int>(NOMINALS::ACE)) && (vCards.size() > 1))
+		{
+			summ += 11;
+		}
+		else
+		{
+			summ +=(*it)->GetValue();
+		}
+    }
+    
+	return summ;
+}
